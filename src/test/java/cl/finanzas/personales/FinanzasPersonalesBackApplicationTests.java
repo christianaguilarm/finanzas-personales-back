8a803f36d159da126a7ddc9e435222e81670dc96
Christian Aguilar <caguilar@advise.cl>
2025-11-09 14:42:34 -0300

    create spring boot project

---
+++ src/test/java/cl/finanzas/personales/FinanzasPersonalesBackApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.finanzas.personales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanzasPersonalesBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
